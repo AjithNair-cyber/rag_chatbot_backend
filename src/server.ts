233fa2390494f6905041b5548f17ca81c1af27bd
AjithNair-cyber <nairajith1507@gmail.com>
2025-09-16 18:54:18 +0400

    added cron

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import app from "./app";
 import CONFIGS from "./config/envConfigs";
 import cron from "node-cron";
+import scrappingService from "./service/scrappingService";
 
 // Start the server
 app.listen(CONFIGS.PORT, async () => {
+  await scrappingService.fetchAndSaveRSS();
   console.log(`Server running at http://localhost:${CONFIGS.PORT}`);
 });
 
 // Runs at 12:00 AM and 12:00 PM every day
-cron.schedule("0 0,12 * * *", () => {
+cron.schedule("0 0,12 * * *", async () => {
   // Your task code here
+  await scrappingService.fetchAndSaveRSS();
   console.log("Task runs at midnight and noon daily");
 });
