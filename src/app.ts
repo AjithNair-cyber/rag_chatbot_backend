2f24015e8ce228834c55f2f76c9d322a98efd9cd
AjithNair-cyber <nairajith1507@gmail.com>
2025-09-16 18:43:59 +0400

    added cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import vectorRouter from "./routes/vectorRoute";
 import errorHandler from "./middlewares/errorHandler";
+import CONFIGS from "./config/envConfigs";
 
 // Initialize Express app
 const app = express();
 
 // Middleware
-// app.use(cors());
+app.use(
+  cors({
+    origin: CONFIGS.UI_URL, // or '*' for testing
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Routes
--- src/config/envConfigs.ts
+++ src/config/envConfigs.ts
@@ -22,6 +22,7 @@ const CONFIGS = {
   REDIS_DATABASE_PASSWORD: process.env.REDIS_DATABASE_PASSWORD || "",
   GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
   GEMINI_API_URL: process.env.GEMINI_API_URL || "",
+  UI_URL: process.env.UI_URL || "",
 };
 
 export default CONFIGS;
